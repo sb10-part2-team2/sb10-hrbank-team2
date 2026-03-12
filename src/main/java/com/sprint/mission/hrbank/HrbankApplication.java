6ead188732c3b9962a66f91478d03eb696bae9f4
conradrado <gimmy6707@gmail.com>
2026-03-12 11:15:52 +0900

    Update: build.gradle 최신화

--- src/main/java/com/sprint/mission/hrbank/HrbankApplication.java
+++ src/main/java/com/sprint/mission/hrbank/HrbankApplication.java
@@ -3,7 +3,7 @@ package com.sprint.mission.hrbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//initial commit
+//initialcommit
 @SpringBootApplication
 public class HrbankApplication {
 
