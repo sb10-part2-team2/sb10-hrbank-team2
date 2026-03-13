b7379baa5b1730dc780e9f97381b30669a40885d
parkngg <millco0803com@gmail.com>
2026-03-13 11:15:14 +0900

    feat BackupRepository 인터페이스 설계
    최신순 정렬 및 상태별 페이징 조회 메서드를 구현했다.
    
    Related to: #23

--- src/main/java/com/sprint/mission/hrbank/domain/backup/repository/BackupRepository.java
+++ src/main/java/com/sprint/mission/hrbank/domain/backup/repository/BackupRepository.java
@@ -1,7 +1,7 @@
-package com.sprint.mission.hrbank.repository;
+package com.sprint.mission.hrbank.domain.backup.repository;
 
-import com.sprint.mission.hrbank.domain.backup.Backup;
-import com.sprint.mission.hrbank.domain.backup.BackupStatus;
+import com.sprint.mission.hrbank.domain.backup.entity.Backup;
+import com.sprint.mission.hrbank.domain.backup.entity.BackupStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +10,6 @@ public interface BackupRepository extends JpaRepository<Backup, Long> {
 
   Page<Backup> findAllByOrderByStartedAtDesc(Pageable pageable); // 전체 백업 이력 최신순 조회
 
-  Page<Backup> findAllByStatusOrderByStartedAtDesc(BackupStatus status, Pageable pageable); // 상태별 백업 이력 필터링 조회
+  Page<Backup> findAllByStatusOrderByStartedAtDesc(BackupStatus status,
+      Pageable pageable); // 상태별 백업 이력 필터링 조회
 }
