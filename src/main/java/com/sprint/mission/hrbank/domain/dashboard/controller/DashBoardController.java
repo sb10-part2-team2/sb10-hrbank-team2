496d40e19a37472ec2d7c8fc029d9fd1d79e56af
CHH01 <alex011003@gmail.com>
2026-03-13 11:15:54 +0900

    fix: 대시보드 엔드포인트 수정
    대시보드 컨트롤러 URI를 API명세에 맞게 변경함.
    
    Related to: #9

--- src/main/java/com/sprint/mission/hrbank/domain/dashboard/controller/DashBoardController.java
+++ src/main/java/com/sprint/mission/hrbank/domain/dashboard/controller/DashBoardController.java
@@ -5,17 +5,15 @@ import com.sprint.mission.hrbank.domain.dashboard.service.DashBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/dashboards")
 @RequiredArgsConstructor
 public class DashBoardController {
 
   private final DashBoardService dashBoardService;
 
-  @GetMapping("/employees/summary")
+  @GetMapping("api/employees/count")
   public ResponseEntity<EmployeeSummaryResponse> getEmployeeSummary() {
     EmployeeSummaryResponse dummydata = dashBoardService.getEmployeeSummary();
     return ResponseEntity.ok(dummydata);
