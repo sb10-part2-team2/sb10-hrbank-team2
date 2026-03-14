3e2deea70da489d8f70e268a71ee360369cf6b0e
raonPsm <s102075@naver.com>
2026-03-14 17:13:02 +0900

    feat: FileRepository 구현
    FileRepository를 구현하였습니다.
    
    Resolves: #35

---
+++ src/main/java/com/sprint/mission/hrbank/domain/file/repository/FileRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.hrbank.domain.file.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository {
+  
+}
