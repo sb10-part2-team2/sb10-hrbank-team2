71054d31445626d6bb3e47d0365f903939ba2611
conradrado <gimmy6707@gmail.com>
2026-03-14 12:32:22 +0900

    fix: 직원 등록 기능 보완 file 생성 후 file 영속화하는 로직을 일단 주석에서 미약하게나마 구현 서비스 계층에 Transaction 어노테이션 추가 fixes: #10

--- src/main/java/com/sprint/mission/hrbank/domain/employee/EmployeeService.java
+++ src/main/java/com/sprint/mission/hrbank/domain/employee/EmployeeService.java
@@ -12,14 +12,17 @@ import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class EmployeeService {
   private final EmployeeRepository employeeRepository;
   private final DepartmentRepository departmentRepository;
   private final EmployeeMapper employeeMapper;
+
 
   public CursorPageResponseEmployeeDto getEmployees(EmployeeSearchRequest req) {
     Objects.requireNonNull(req, ("유효하지 않은 요청!"));
@@ -30,6 +33,7 @@ public class EmployeeService {
     String sortDirection = req.sortDirection() == null ? "asc" : req.sortDirection();
     return // TODO: 추후 구현 예정
   }
+
 
   public EmployeeDto create(EmployeeCreateRequest req, MultipartFile profile) {
     Objects.requireNonNull(req, "유효하지 않은 요청입니다!");
@@ -42,11 +46,15 @@ public class EmployeeService {
 
     // String으로 들어온 입사일을 LocalDate로 전환함.
     LocalDate hireDate = LocalDate.parse(req.hireDate());
-    //File file = null;
+
+    File file = null;
     //TODO: 추후 FILE 부분 완성이 되면 구현 예정입니다.
-//     if (profile != null) {
-//       file = new File();
-//     }
+     if (profile != null) {
+//         file = new File();
+
+    //  file 영속화
+    //  fileRepository.save(file);
+     }
 
     Employee employee = new Employee(
         req.name(),
@@ -57,7 +65,8 @@ public class EmployeeService {
         file
     );
 
+    // 새로 생성된 유저 엔티티를 영속화함.
     Employee saved = employeeRepository.save(employee); // 레포지토리 인터페이스를 통해 영속화
-      return employeeMapper.entityToDto(saved);
+      return employeeMapper.entityToDto(saved); // 그 후 employeeDto 형식으로 리턴.ㅎ
     }
 }
