3e167f6c9cf2365f520b380a8c0fbaa94b712721
Amperisk9 <amperisk9@gmail.com>
2026-03-16 00:43:56 +0900

    fix: 함수 파라미터 annotation 추가
    Query문의 id와 repository 함수의 파라미터와 다른단 리뷰를 받고
    @param annotation을 추가하였습니다
    
    Fixes: #34

--- src/main/java/com/sprint/mission/hrbank/domain/department/DepartmentRepository.java
+++ src/main/java/com/sprint/mission/hrbank/domain/department/DepartmentRepository.java
@@ -3,6 +3,7 @@ package com.sprint.mission.hrbank.domain.department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
@@ -11,5 +12,5 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
   @Modifying(clearAutomatically = true)       // 쿼리 실행 후 1차 캐시 비워줌
   @Query("DELETE FROM Department d WHERE d.id = :id AND NOT EXISTS "
       + "(SELECT e FROM Employee e WHERE e.department.id = :id)")
-  int deleteByIdIfNotEmployee(Long departmentId);
+  int deleteByIdIfNotEmployee(@Param("id") Long departmentId);
 }
