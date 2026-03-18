a74b2f2f7377a3a1999e33365b7b575c52123cc7
Amperisk9 <amperisk9@gmail.com>
2026-03-18 13:34:45 +0900

    fix: 에러반환 코드 변경
    부서가 존재하지 않는 에러의 반환타입을 400에서 404로 변경하였습니다
    
    Resolves: #96

--- src/main/java/com/sprint/mission/hrbank/global/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/hrbank/global/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
   // 대시보드
 
   // 부서
-  DEPARTMENT_NOT_FOUND(400, "해당하는 부서가 없습니다"),
+  DEPARTMENT_NOT_FOUND(404, "해당하는 부서가 없습니다"),
   DEPARTMENT_NAME_DUPLICATE(400, "동일한 이름을 가진 부서가 있습니다"),
   DEPARTMENT_NOT_DELETABLE(400, "부서에 소속된 직원이 있어서 삭제할 수 없습니다"),
 
